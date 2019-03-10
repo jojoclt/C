bd19cc6b9b3e20d8bdbc1557c9ad643c3ebb1d6a
Chalanthorn C <Chalanthorn_jojo@Hotmail.com>
2019-03-10 22:27:33 +0700

    Create 157_MoneyCoupon.cpp

---
+++ codecube/157_MoneyCoupon.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+char cc;
+int t,n,i;
+long long a,b,c = 1;
+bool check;
+int main(){
+    scanf("%d",&n);
+    for (i = 0; i < n; i++){
+        scanf(" %c%d",&cc,&t);
+        if (cc == '+') a += t;
+        else if (cc == '-') b += t;
+        else if (cc == 'x'){
+            if (!t) check = true;
+            else c *= t;
+        }
+    }
+    if (check) b = 0;
+    printf("%lld",a*c-b);
+}
