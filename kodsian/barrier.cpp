74f75e7e200e84945dbe14e33eab5a2280aa217b
jojojmtk <Chalanthorn_jojo@hotmail.com>
2019-04-12 15:40:40 +0700

    Update
    AC

--- kodsian/barrier.cpp
+++ kodsian/barrier.cpp
@@ -1,18 +1,71 @@
 #include <bits/stdc++.h>
-using namespace std;
 using ll = long long;
-int i;
-int a[60000005];
+using namespace std;
+int n,w,i,t1,t2;
+const int MAXN = 6000005;
+int a[MAXN];
 struct Node{
-    ll sum, prefixsum, suffixsum, maxsum;
+    ll sum, maxleftsum, maxrightsum, maxsum;
+    int l,r,len;
     Node(){
-        prefixsum = suffixsum = maxsum = LLONG_MIN;
         sum = 0;
+        maxleftsum = maxrightsum = maxsum = LLONG_MIN;
+        l = r = len = 1;
     }
-};
-int n,w;
+
+}tree[MAXN<<2],res;
+int f(int x){
+    return 1 << (int)(ceil(log2(n)) - floor(log2(x)));
+}
+Node Merge(Node l, Node r, int x){
+    Node p;
+    p.sum = l.sum + r.sum;
+    p.maxleftsum = max(l.maxleftsum, l.sum + r.maxleftsum);
+    p.maxrightsum = max(r.maxrightsum, r.sum + l.maxrightsum);
+    p.maxsum = max({l.maxsum, r.maxsum, l.maxrightsum + r.maxleftsum});
+
+    if (p.maxleftsum == l.maxleftsum) p.l = l.l;
+    else p.l = x + r.l;
+    if (p.maxrightsum == r.maxrightsum) p.r = r.r;
+    else p.r = x + l.r;
+
+    if (p.maxsum == l.maxsum) p.len = l.len;
+    else if (p.maxsum == r.maxsum) p.len = r.len;
+    else p.len = l.r+r.l;
+    return p;
+}
+void build(int node, int s, int e){
+    if (s == e) {
+        tree[node].sum = tree[node].maxleftsum = tree[node].maxrightsum = tree[node].maxsum = a[s];
+        return;
+    }
+    int mid = s+e>>1;
+    build(node<<1,s,mid);
+    build(node<<1|1,mid+1,e);
+    tree[node] = Merge(tree[node<<1],tree[node<<1|1],f(node<<1));
+}
+Node query(int node, int s, int e, int l, int r){
+    if (e < l || r < s) {
+        Node nullNode;
+        return nullNode;
+        // return (Node){0,MIN,MIN,MIN};
+    }
+    if (l <= s && e <= r) return tree[node];
+    int mid = s+e>>1;
+    Node left = query(node<<1,s,mid,l,r);
+    Node right = query(node<<1|1,mid+1,e,l,r);
+    Node result = Merge(left,right,f(node<<1));
+    return result;
+}
 int main(){
-    scanf("%d%d",&n,&w);
+    scanf("%d",&n);
+    scanf("%d",&w);
     for (i = 0; i < n; i++) scanf("%d",&a[i]);
-    // build(1,0,n-1);
+    build(1,0,n-1);
+//    for (i = 1; i < 2*n; i++) printf("%d: %d\n",i,f(i));
+//    return 0;
+    for (i = 0; i < n-w+1; i++){
+        res = query(1,0,n-1,i,i+w-1);
+        printf("%lld\n%d",res.maxsum,res.len);
+    }
 }
