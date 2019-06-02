ea7cf840284917ecf568c2dcb3b72ebc46b9ae6f
jojojmtk <Chalanthorn_jojo@hotmail.com>
2019-06-02 17:25:34 +0700

    OK

--- kodsian/barrier2019.cpp
+++ kodsian/barrier2019.cpp
@@ -2,23 +2,22 @@
 using namespace std;
 using ll = long long;
 int n,k,i;
-ll a[6000005];
-struct node{
-    int x; ll val;
-}t;
+ll a[6000001];
+struct node{ int x; ll val; };
 deque<node>dq;
-int _max,cnt;
+ll _max; int cnt;
 int main(){
     scanf("%d%d",&n,&k);
     for (i = 1; i <= n; i++) scanf("%lld",&a[i]),a[i] += a[i-1];
-    for (i = 1; i <= n; i++){
-        int tx,tval;
-        while (!dq.empty() && i-dq.front().x >= k) dq.pop_front();
+    dq.push_front({1,0});
+    for (i = 2; i <= n; i++){
+        int tx;ll tval;
+        while (i-dq.front().x >= k) dq.pop_front();
         while (!dq.empty() && dq.back().val >= a[i-1]) dq.pop_back();
         dq.push_back({i,a[i-1]});
-        tval = a[i]-dq.front().val;
-        if (_max < tval) _max = tval,cnt = i-dq.front().x+1;
-        else if (_max == tval) cnt = min(cnt,i-dq.front().x+1);
+        tx = i-dq.front().x+1, tval = a[i]-dq.front().val;
+        if (_max < tval) _max = tval,cnt = tx;
+        else if (_max == tval) cnt = min(cnt,tx);
     }
     printf("%lld\n%d",_max,cnt);
 }
